7bcd07d333cbbccc9d8dc7da84b89ed13d297e32
Petr Filaretov <p.filaretov@okko.tv>
2023-07-15 20:04:19 +0400

    run the game - simple recalculate of the field

--- src/main/java/dev/pfilaretov42/glue/LifeField.java
+++ src/main/java/dev/pfilaretov42/glue/LifeField.java
@@ -1,14 +1,20 @@
 package dev.pfilaretov42.glue;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static dev.pfilaretov42.glue.GlueApplication.*;
 
 @Component
 public class LifeField {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LifeField.class);
 
     private final LifeActionListener lifeActionListener;
     private final JButton[][] field = new JButton[ROWS][COLUMNS];
@@ -41,8 +47,32 @@ public class LifeField {
         }
     }
 
+    @SuppressWarnings("java:S2189")
     public void run() {
-        // TODO - run the game
-        field[0][0].setBackground(Color.BLACK);
+        try (ExecutorService executor = Executors.newCachedThreadPool()) {
+            executor.submit(() -> {
+                // calculate everything in a single thread
+                LOG.info("calculating...");
+                // TODO - app hangs with while
+                while (true) {
+                    for (int i = 0; i < ROWS; i++) {
+                        for (int j = 0; j < COLUMNS; j++) {
+                            Color background = field[i][j].getBackground();
+                            boolean isLife = background == COLOR_LIFE;
+                            if (isLife) {
+                                field[i][j].setBackground(COLOR_NO_LIFE);
+                            } else {
+                                field[i][j].setBackground(COLOR_LIFE);
+                            }
+                        }
+                    }
+
+                    Thread.sleep(5000);
+                }
+            });
+
+            LOG.info("Execution submitted");
+        }
+
     }
 }
