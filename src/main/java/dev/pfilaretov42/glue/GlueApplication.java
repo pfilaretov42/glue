054cb51db60525a395b90dc7185a94af6cf7d574
Petr Filaretov <p.filaretov@okko.tv>
2023-07-15 17:51:39 +0400

    initial commit

---
+++ src/main/java/dev/pfilaretov42/glue/GlueApplication.java
@@ -0,0 +1,13 @@
+package dev.pfilaretov42.glue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pfilaretov42/glue/GlueApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pfilaretov42.glue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
